b0b2a67eaeedb5eadcd1f2ba23ed35244ab5032f
SUHWAN KIM <oink12268@gmail.com>
2021-12-26 13:03:22 +0900

    .

--- src/main/java/com/happy/lovely/LovelyApplication.java
+++ src/main/java/com/happy/lovely/LovelyApplication.java
@@ -1,13 +1,21 @@
 package com.happy.lovely;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class LovelyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LovelyApplication.class, args);
+
+        log.info("*----------------*");
+        log.info("|                |");
+        log.info("|   SUCCESS !!!  |");
+        log.info("|                |");
+        log.info("*----------------*");
     }
 
 }
