758d7b202167fef896f53bd04aad00576bb1c7a7
SUHWAN KIM <oink12268@gmail.com>
2021-12-26 12:52:21 +0900

    first commit

---
+++ src/main/java/com/happy/lovely/LovelyApplication.java
@@ -0,0 +1,13 @@
+package com.happy.lovely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/happy/lovely/LovelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.lovely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
