8201e32d1b0273ce756a0172d35a68a5c04c59b2
SUHWAN KIM <oink12268@gmail.com>
2022-01-19 22:45:12 +0900

    로그인시 localstorage load

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authorization')
+
+    if (token) {
+      config.headers.Authorization = token
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 createApp(App).use(store).use(router).mount('#app')
